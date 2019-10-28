1032ce8c941175f4b4fb0dd5ec12cd56a47fceb5
achall9 <alexandros.hall9@gmail.com>
2019-10-28 10:53:42 -0400

    change config

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
 export default {
-    //serverUrl : 'https://next-celeste.herokuapp.com/',
-    serverUrl : 'http://localhost:4000/'
+    serverUrl : 'https://next-celeste.herokuapp.com/',
+    //serverUrl : 'http://localhost:4000/'
 }
