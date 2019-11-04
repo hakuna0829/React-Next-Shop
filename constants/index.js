907cf70e1263c675b9d911e81101d868c0547fb3
achall9 <alexandros.hall9@gmail.com>
2019-11-04 12:48:14 -0500

    server url update

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,5 @@
 export default {
-    serverUrl : 'https://next-celeste.herokuapp.com/',
+    //serverUrl : 'https://next-celeste.herokuapp.com/',
+    serverUrl : 'http://174.138.39.249/',
     //serverUrl : 'http://localhost:4000/'
 }
