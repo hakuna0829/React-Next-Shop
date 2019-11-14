96a8007936ab3c6b41d26073399bf796c2bc640b
achall9 <alexandros.hall9@gmail.com>
2019-11-14 14:08:11 -0500

    profile checklist page update

--- components/Layout.js
+++ components/Layout.js
@@ -19,6 +19,7 @@ export default ({ children, title = 'Celeste'}) => (
         <link rel="stylesheet" href="/css/login.css" />
         <link rel="stylesheet" href="/css/navbar.css" />
         <link rel="stylesheet" href="/css/create-profile.css" />
+        <link rel="stylesheet" href="/css/profile_checklist.css" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
