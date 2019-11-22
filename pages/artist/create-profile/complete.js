b9f0faaefd323fbda6784b68949a5907dbfb2588
kyle-toplev <kyle@toplev.io>
2019-11-22 22:14:30 +0700

    add policies screens except availability

--- pages/artist/create-profile/complete.js
+++ pages/artist/create-profile/complete.js
@@ -1,50 +1,79 @@
-import React from 'react';
-import Router from 'next/router';
-import axios from 'axios';
+import React from "react";
+import Router from "next/router";
+import axios from "axios";
 
-import Layout from '../../../components/Layout';
+import Layout from "../../../components/Layout";
 
-import constants from '../../../constants';
+import constants from "../../../constants";
 
 class CompletePage extends React.Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    changePublicState = (is_public) => {
-        let token = this.props.token
-
-        axios.put(constants.serverUrl + 'api/profiles/me/updatePublicState', { is_public }, { headers: { 'Authorization': token } })
-          .then((response) => {
-            Router.push('/artist/dashboard')
-          })
-          .catch((error) => {
-            console.log(error)
-          })
-    }
-
-    render() {
-        return (
-            <Layout title={'Complete'}>
-                <div className="suggest">
-                    <h1> Complete </h1>
-                    <div className="page-navs">
-                        <button type="button" className="btn btn-primary ellipsis" onClick={() => {this.changePublicState(true)}}> Go Public </button>
-                        <button type="button" className="btn btn-primary ellipsis" onClick={() => {this.changePublicState(false)}}> Stay Private </button>
-                    </div>
-                </div>
-                <style jsx>{`
-                    .suggest {
-                        text-align: center;
-                    }
-                    .page-navs a {
-                        margin-right: 30px;
-                    }
-                `}</style>
-            </Layout>
-        );
-    }
+  constructor(props) {
+    super(props);
   }
-  
-  export default CompletePage;
+
+  changePublicState = is_public => {
+    let token = this.props.token;
+
+    axios
+      .put(
+        constants.serverUrl + "api/profiles/me/updatePublicState",
+        { is_public },
+        { headers: { Authorization: token } }
+      )
+      .then(response => {
+        Router.push("/artist/dashboard");
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <Layout title={"Complete"}>
+        <div className="profile">
+          <div className="container">
+            <div className="row smallrow">
+              <center><h3>Your all set! </h3></center>
+              <p>
+                Your shop is all ready to go! you can make your shop public and
+                start accepting requrest for bookings or you can keep it private
+                for now.
+              </p>
+              <div className="page-navs">
+                <div className="column-2-space">
+                  <span className="button">
+                    <button
+                      type="button"
+                      className="btn btn-primary ellipsis btn-block"
+                      onClick={() => {
+                        this.changePublicState(true);
+                      }}
+                    >
+                      {" "}
+                      Go Public{" "}
+                    </button>
+                  </span>
+                  <span className="button">
+                    <button
+                      type="button"
+                      className="btn btn-primary ellipsis btn-block"
+                      onClick={() => {
+                        this.changePublicState(false);
+                      }}
+                    >
+                      {" "}
+                      Stay Private{" "}
+                    </button>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default CompletePage;
